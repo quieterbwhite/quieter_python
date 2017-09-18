b9adf6397115a011e23aa664a2397d273a218999
bwhite <bwhite.liiiii@gmail.com>
2017-09-18 13:33:38 +0800

    mysql

---
+++ mysql/04_driver.py
@@ -0,0 +1,26 @@
+# -*- coding=utf-8 -*-
+# Created Time: 2017年09月17日 星期日 16时36分15秒
+
+"""
+mysql driver
+"""
+
+
+"""
+Install:
+
+    python2:
+
+        apt-get install python-dev, libmysqlclient-dev, python-pip
+
+        pip install mysql-python
+
+        import MySQLdb
+
+    python3:
+
+        pysql ?
+
+
+Connection:
+"""
