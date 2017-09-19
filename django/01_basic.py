96462ee533c22c43cda34490230ae6f7cf0bc905
bwhite <bwhite.liiiii@gmail.com>
2017-09-20 00:08:33 +0800

    django
    django

--- django/01_basic.py
+++ django/01_basic.py
@@ -4,25 +4,57 @@
 
 """
 
-pip install django
+创建项目
 
-django-admin startproject dream
+    安装django
+    pip install django
 
-cd dream
+    创建项目
+    django-admin startproject dream
+    cd dream
 
-python manage.py startapp tiger
+    创建应用
+    python manage.py startapp tiger
 
-add model info
+    注册应用
+    add app info to INSTALL_APPS
 
-python manage.py makemigrations
+    添加模型信息
+    add model info
 
-python manage.py migrate
+    生成迁移命令
+    python manage.py makemigrations
 
-python shell:
+    迁移
+    python manage.py migrate
 
-    from tiger.models import *
+    开发服务器运行项目
+    python manage.py runserver 0.0.0.0:8000
 
-    b = BookInfo()
+使用django的管理
+
+    创建一个管理员用户
+    python manage.py createsuperuser，按提示输入用户名、邮箱、密码
+
+    启动服务器，通过“127.0.0.1:8000/admin”访问，输入上面创建的用户名、密码完成登录
+    进入管理站点，默认可以对groups、users进行管理
+
+管理界面本地化
+
+    编辑settings.py文件，设置编码、时区
+    LANGUAGE_CODE = 'zh-Hans'
+    TIME_ZONE = 'Asia/Shanghai'
+
+向admin注册booktest的模型
+
+    打开booktest/admin.py文件，注册模型
+    from django.contrib import admin
+    from models import BookInfo
+    admin.site.register(BookInfo)
+
+    刷新管理页面，可以对BookInfo的数据进行增删改查操作
+    问题：如果在str方法中返回中文，在修改和添加时会报ascii的错误
+    解决：在str()方法中，将字符串末尾添加“.encode('utf-8')”
 
 
 """
--- django/dream/tiger/models.py
+++ django/dream/tiger/models.py
@@ -6,12 +6,21 @@ from django.db import models
 
 class BookInfo(models.Model):
 
+    # 类变量，映射表结构
     btitle = models.CharField(max_length=20)
 
+    # 对应 python datetime 类型
     bpub_date = models.DateField()
 
     def __str__(self):
         return self.btitle
+
+    """
+        b = BookInfo()
+        b.btitle = "abc"
+        # 这里的实例属性并不是在调用上面定义的类变量，只是动态绑定了一个同名的属性
+        # 将来在 save() 的时候，就知道sql语句中应该用什么字段名。
+    """
 
 
 class HeroInfo(models.Model):
