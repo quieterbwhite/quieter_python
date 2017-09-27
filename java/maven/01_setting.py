baae5fa36093579d85be64b18ec83cb62fde36f4
bwhite <bwhite.liiiii@gmail.com>
2017-09-27 23:02:01 +0800

    add cate java

---
+++ java/maven/01_setting.py
@@ -0,0 +1,17 @@
+# -*- coding=utf-8 -*-
+# Created Time: 2017年08月07日 星期一 17时01分06秒
+# File Name: 01_setting.py
+
+"""
+1. 修改默认 maven 源, maven 的 setting.xml 中加入以下配置
+
+    <mirror>
+      <id>nexus-aliyun</id>
+      <mirrorOf>central</mirrorOf>
+      <name>Nexus aliyun</name>
+      <url>http://maven.aliyun.com/nexus/content/groups/public</url>
+    </mirror>
+
+  </mirrors>
+
+"""
---
+++ java/maven/03_my_pom.py
@@ -0,0 +1,42 @@
+# -*- coding=utf-8 -*-
+# Created Time: 2017年08月09日 星期三 17时52分18秒
+# File Name: 02_my_pom.py
+
+如果不想进行全局配置，也可以在项目中单独设置资源库，在pom.xml中加入以下配置
+
+    <build>
+        <plugins>
+            <plugin>
+                <groupId>org.springframework.boot</groupId>
+                <artifactId>spring-boot-maven-plugin</artifactId>
+            </plugin>
+            <plugin>
+                <artifactId>maven-compiler-plugin</artifactId>
+                <configuration>
+                    <source>1.8</source>
+                    <target>1.8</target>
+                    <encoding>UTF-8</encoding>
+                </configuration>
+            </plugin>
+        </plugins>
+    </build>
+
+    <repositories>
+        <repository>
+            <id>aliyun-repos</id>
+            <url>http://maven.aliyun.com/nexus/content/groups/public/</url>
+            <snapshots>
+                <enabled>false</enabled>
+            </snapshots>
+        </repository>
+    </repositories>
+
+    <pluginRepositories>
+        <pluginRepository>
+            <id>aliyun-plugin</id>
+            <url>http://maven.aliyun.com/nexus/content/groups/public/</url>
+            <snapshots>
+                <enabled>false</enabled>
+            </snapshots>
+        </pluginRepository>
+    </pluginRepositories>
