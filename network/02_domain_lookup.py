e2d45e34c73741cea0417fd755d053a70d9c61fc
bwhite <bwhite.liiiii@gmail.com>
2017-10-28 14:57:10 +0800

    domain

---
+++ network/02_domain_lookup.py
@@ -0,0 +1,29 @@
+# -*- coding=utf-8 -*-
+# Created Time: 2017年10月16日 星期一 21时36分47秒
+# File Name: 02_domain_lookup.py
+
+"""
+
+chrome -> chrome_cache -> /etc/hosts -> os_cache -> dianxin -> root_server
+
+-> gTLD(13 nodes global) -> Got it
+
+"""
+
+"""
+
+dns cache depends on:
+
+    TTL
+
+    cache space
+
+"""
+
+"""
+
+delete cache on os:
+
+    /etc/init.d/nscd restart
+
+"""
