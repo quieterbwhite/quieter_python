004f6d58191e38ff974d5785a9cd259672fa95c9
libo <libo@swartz.com>
2017-12-05 17:40:59 +0800

    add pip config

---
+++ python_attr/08_pip.py
@@ -0,0 +1,19 @@
+# -*- coding=utf-8 -*-
+# Created Time: 2017年12月05日 星期二 17时39分48秒
+# File Name: 08_pip.py
+
+"""
+1. 安装 pip
+
+sudo apt-get install python-pip
+2. 修改 pip 源
+
+ 根目录新建或修改 pip 配置文件
+
+ $ touch ~/.pip/pip.conf
+ 输入如下内容即可:
+
+     [global]
+     trusted-host = mirrors.aliyun.com
+     index-url = http://mirrors.aliyun.com/pypi/simple/
+"""
