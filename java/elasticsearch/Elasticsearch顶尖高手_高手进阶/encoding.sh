c8addd3993d0f7eace65576beaca60ed8df61891
bwhite <bwhite.liiiii@gmail.com>
2019-08-31 20:51:48 +0800

    elasticsearch

---
+++ java/elasticsearch/Elasticsearch顶尖高手_高手进阶/encoding.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+### 将 values_here 替换为输入编码
+FROM_ENCODING="gb18030"
+### 输出编码 (UTF-8)
+TO_ENCODING="UTF-8"
+### 转换命令
+CONVERT=" iconv  -f   $FROM_ENCODING  -t   $TO_ENCODING"
+### 使用循环转换多个文件
+for  file  in  *.txt; do
+$CONVERT   "$file"   -o  "${file%.txt}"
+done
+exit 0
